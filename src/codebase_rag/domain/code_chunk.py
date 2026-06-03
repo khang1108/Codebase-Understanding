3c83fc5413b948a6caa31102f1658111c80a08dc
Nguyen Phuc Khang <nguyenphuc.khang110806@gmail.com>
2026-06-03 10:25:22 +0700

    feat: add a new Code Chunk Class for the system. Mr.Kien will use this to generate chunks

---
+++ src/codebase_rag/domain/code_chunk.py
@@ -0,0 +1,24 @@
+"""
+A dataclass for a chunk of source code.
+
+This data model is the primary unit of data exchanged between the ingestion layer and the retrieval layer.
+"""
+
+from __future__ import annotations
+from pydantic import BaseModel, Field
+
+class CodeChunk(BaseModel):
+    """
+    CodeChunk là data model thống nhất chứa các thông tin cho một chunk source code bất kỳ. 
+
+    Attributes:
+        chunk
+    """
+    # Lưu chunk_id theo dạng đường dẫn và dòng sẽ bổ sung đầy đủ thông tin hơn cho một chunk, thay vì là những có số random.
+    chunk_id: str = Field(examples="path/to/your/file/src.py:from_line-end_line")
+    file_path: str 
+    language: str # Ngôn ngữ mà đoạn code đó dùng
+    start_line: int = Field(ge=1)
+    end_line: int = Field(ge=1)
+    text: str # Nội dung đoạn code
+    repo_name: str | None = None # thuộc về repo nào. Có thể lưu hoặc không lưu
